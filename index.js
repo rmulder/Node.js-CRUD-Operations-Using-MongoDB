1aa4356b4deb9f3b454aff6f8fd624fd478775e6
Michał Dziedziński <michal.marcin.dziedzinski@gmail.com>
2018-11-08 18:52:15 +0100

    Pagination

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ async function createCourse() {
 }
 
 async function getCourses() {
+    const pageNumber = 2;
+    const pageSize = 10;
     // eq (equal)
     // ne (not equal)
     // gt (greater than or equal to)
@@ -50,6 +52,7 @@ async function getCourses() {
 
         // .or([{author: 'Mosh'}, {isPublished: true}])
         // .and([{author: 'Mosh'}, {isPublished: true}])
+        .skip((pageNumber - 1) * pageSize)
         .limit(10)
         .sort({name: 1})
         // .select({name: 1, tags: 1})
