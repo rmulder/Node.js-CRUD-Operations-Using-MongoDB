58242e50735d8537455bd86806631f1812479b74
Michał Dziedziński <michal.marcin.dziedzinski@gmail.com>
2018-11-17 09:58:41 +0100

    Validation

--- index.js
+++ index.js
@@ -1,57 +1,47 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground', {useNewUrlParser: true})
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB...', err));
 
 const courseSchema = new mongoose.Schema({
-    name: String,
+    name: {type: String, required: true},
     author: String,
     tags: [String],
     date: {type: Date, default: Date.now},
-    isPublished: Boolean
+    isPublished: Boolean,
+    price: Number
 });
 
 const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
     const course = new Course({
-        name: 'Node Course',
+        // name: 'Node Course',
         author: 'Mosh',
         tags: ['node', 'backend'],
-        isPublished: true
+        isPublished: true,
+        price: 15
     });
-    const result = await course.save();
-    console.log(result);
+    try{
+        const result = await course.save();
+        console.log(result);
+    }
+    catch(ex){
+        console.log(ex.message);
+    }
 }
 
 async function getCourses() {
     const pageNumber = 2;
     const pageSize = 10;
-    // eq (equal)
-    // ne (not equal)
-    // gt (greater than or equal to)
-    // gte (greater than or equal to)
-    // lt (less than)
-    // lte (less than or equal to)
-    // in
-    // nin (not in)
 
     const courses = await Course
         .find({ author: 'Mosh', isPublished: true})
-        // .find({price: { $gte: 10, $lte: 20}})
-        // .find({price: {$in: [10, 15, 20]}})
-        //Starts with Mosh
-        // .find([{author: /^Mosh/}])
-        // .find([{author: /Hamedani$/i}])
-        // .find([{author: /.*Mosh.*/i}])
-        // .or([{author: 'Mosh'}, {isPublished: true}])
-        // .and([{author: 'Mosh'}, {isPublished: true}])
         .skip((pageNumber - 1) * pageSize)
         .limit(pageSize)
         .sort({name: 1})
         .select({name: 1, tags: 1});
-        // .countDocuments();
     console.log(courses);
 }
 
@@ -66,9 +56,8 @@ async function updateCourse(id) {
 }
 
 async function removeCourse(id) {
-//    const result = await Course.deleteOne({ _id: id });
     const course = Course.findOneAndRemove(id);
     console.log(course);
 }
 
-removeCourse('5be472e08b98a405e49119fc');
+createCourse();
